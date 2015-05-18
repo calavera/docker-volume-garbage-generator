f64994e52696add74511c1d2db0a61b205b95eb8
David Calavera <david.calavera@gmail.com>
2015-05-18 15:56:59 -0700

    :sunrise:
    Signed-off-by: David Calavera <david.calavera@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Docker volume garbage generator
+
+An example of the [Docker volume extension api](https://github.com/calavera/docker-volume-api)
+
+Docker volume extension that mounts garbage into your containers.
+
+## License
+
+MIT
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/calavera/docker-volume-api"
+)
+
+type garbageDriver struct{}
+
+func (g garbageDriver) Create(r volumeapi.VolumeRequest) volumeapi.VolumeResponse {
+	return volumeapi.VolumeResponse{}
+}
+
+func (g garbageDriver) Remove(r volumeapi.VolumeRequest) volumeapi.VolumeResponse {
+	return volumeapi.VolumeResponse{}
+}
+
+func (g garbageDriver) Path(r volumeapi.VolumeRequest) volumeapi.VolumeResponse {
+	return volumeapi.VolumeResponse{Mountpoint: filepath.Join(r.Root, r.Name)}
+}
+
+func (g garbageDriver) Mount(r volumeapi.VolumeRequest) volumeapi.VolumeResponse {
+	p := filepath.Join(r.Root, r.Name)
+
+	if err := os.MkdirAll(p, 0755); err != nil {
+		return volumeapi.VolumeResponse{Err: err}
+	}
+
+	if err := ioutil.WriteFile(filepath.Join(p, "test"), []byte("TESTTEST"), 0644); err != nil {
+		return volumeapi.VolumeResponse{Err: err}
+	}
+
+	return volumeapi.VolumeResponse{Mountpoint: p}
+}
+
+func (g garbageDriver) Umount(r volumeapi.VolumeRequest) volumeapi.VolumeResponse {
+	p := filepath.Join(r.Root, r.Name)
+
+	err := os.RemoveAll(p)
+	return volumeapi.VolumeResponse{Err: err}
+}
+
+func main() {
+	d := garbageDriver{}
+	h := volumeapi.NewVolumeHandler(d)
+	fmt.Println("Listening on :7878")
+	fmt.Println(h.ListenAndServe(":7878"))
+}
